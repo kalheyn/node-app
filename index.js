9ee29dd34d9f5e2bd6acbc38e66ed7b61c0e9c7e
kalheyn <kalheyn@gmail.com>
2020-12-26 09:36:16 -0800

    Set Joi objectId validation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('./startup/routes')(app); 
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));  
