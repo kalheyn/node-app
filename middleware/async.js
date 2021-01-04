b665e6302e26e7df265f6664d17e241ddb304e37
kalheyn <kalheyn@gmail.com>
2021-01-04 01:10:09 -0800

    monkeypatch async error handling

--- middleware/async.js
+++ middleware/async.js
@@ -1,5 +1,8 @@
+// npm module express-async-errors automatically "monkeypatches" this into the routes
 
-module.exports = function (handler) { // handler is a reference to an async function 
+module.exports = function (handler) { 
+    // handler is a reference to an async function: async (req, res) => {}    
+
     return async (req, res, next) => {
         try{
             await handler(req, res); 
